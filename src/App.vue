ed3aa64301cfb2cea82b3b0ccfd35d5e7c8e6485
SJ <vince15dk@gmail.com>
2019-02-12 18:11:33 +0900

    Youtube API added

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
   <div>
     <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList></Videolist>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import SearchBar from './components/SearchBar'
+import {apiKey} from '../config/config.js'
+import VideoList from './components/VideoList'
 
+const API_KEY = apiKey
 export default {
   name: 'App',
   components: {
-      SearchBar
+      SearchBar,
+      VideoList
   },
   methods: {
       onTermChange(searchTerm){
-          console.log(searchTerm)
+          axios.get('https://www.googleapis.com/youtube/v3/search', {
+              params: {
+                  key: API_KEY,
+                  type: 'video',
+                  part: 'snippet',
+                  q: searchTerm
+              }
+          }).then(response => console.log(response));
       }
   }
 };
